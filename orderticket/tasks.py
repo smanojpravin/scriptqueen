574392ecdc607f9bc3799a5f96901ec4461a96ee
vinosa75 <vinosa75@gmail.com>
2022-10-31 11:18:17 +0530

    expiry updated

--- orderticket/tasks.py
+++ orderticket/tasks.py
@@ -473,7 +473,7 @@ def create_currency():
         try:
  
             # print("inside monthend")
-            expiry = "03-Nov-2022"
+            expiry = "24-Nov-2022"
             dte = dt.strptime(expiry, '%d-%b-%Y')
 
             # print("After exception")
